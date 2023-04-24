cdf6de55ca437b78b4ec76714a489590229564de
Gadzhiev Ruslan <ruslan688@mail.ru>
2023-04-24 18:55:54 +0300

    first_pravki

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import logging
 
 from aiogram import Bot, Dispatcher
 from config_data.config import Config, load_config
-from handlers import other_handlers, user_handlers
+from handlers import user_handlers
 
 # Инициализируем логгер
 logger = logging.getLogger(__name__)
@@ -30,7 +30,7 @@ async def main():
 
     # Регистриуем роутеры в диспетчере
     dp.include_router(user_handlers.router)
-    dp.include_router(other_handlers.router)
+    # dp.include_router(other_handlers.router)
 
     # Пропускаем накопившиеся апдейты и запускаем polling
     await bot.delete_webhook(drop_pending_updates=True)
